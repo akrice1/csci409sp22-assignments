22dacb4fcf60ef41cf4304c8d1047c239e21273a
Aaron <akrice1@coastal.edu>
2022-03-16 21:24:35 -0400

    Week 9 Assignment

--- airports/views.py
+++ airports/views.py
@@ -1,9 +1,18 @@
 from django.http import HttpResponse
-
+from .models import Airport
 def index(request):
-    return HttpResponse('Hello from airports');
+    # Fetch all airports from database
+    airports = Airport.objects.all()
+    # To show some data.
+    airport_list = ', '.join([a.airport_code for a in airports])
+    return HttpResponse('Showing all airports: ' + airport_list);
 
 # Create your views here.
 def airport_info(request, airport_code):
-    return HttpResponse('Showing info for airport: ' + airport_code)
+    # Fetch the airport by a certain code.
+    # We are only expecting one airport per code so we will use get().
+    airport = Airport.objects.get(airport_code=airport_code)
+    # Display the airport name and code.
+    return HttpResponse('Showing info for airport: ' + airport.name + "- " + airport.airport_code)
+
 
